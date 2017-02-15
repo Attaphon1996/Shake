021b4ea4029c1d931d8acc41f42c32361aafad64
อรรถพล วงษ์วิกิจการ <boatbright00@gmail.com>
2017-02-15 20:49:37 +0700

    Initial Commit

---
+++ HW2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW2
+//
+//  Created by อรรถพล วงษ์วิกิจการ on 2/15/2560 BE.
+//  Copyright © 2560 อรรถพล วงษ์วิกิจการ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
